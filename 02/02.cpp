4014a6ea760f1caf4a5e5026f1b8cade9b0451f5
Charles Hood <chood@chood.net>
2024-12-02 16:14:39 -0600

    Speed up parser

--- 02/02.cpp
+++ 02/02.cpp
@@ -12,34 +12,32 @@
 #define MAX_REPORT_COUNT 500000
 #define MAX_REPORT_LEVEL_COUNT 1000
 
-static void get_input(int, char **);
-static void parse_input(void);
-
 struct Report {
 	int level_count;
 	int32_t levels[MAX_REPORT_LEVEL_COUNT];
 };
 
+static void get_input(int, char **);
+static void get_next_report(Report *, ptrdiff_t *);
+
 static ptrdiff_t input_length = 0;
 static char const *input = NULL;
-
-static int report_count = 0;
-static Report reports[MAX_REPORT_COUNT] = {0};
 
 int
 main(int argc, char **argv)
 {
 	get_input(argc, argv);
-	parse_input();
 
 #ifdef SILVER
 	int safe_report_count = 0;
-	for (int i = 0; i < report_count; ++i) {
-		Report const *r = &reports[i];
-		bool inc = r->levels[0] < r->levels[1];
-		for (int j = 0; j < r->level_count - 1; ++j) {
-			int32_t a = r->levels[j];
-			int32_t b = r->levels[j + 1];
+	for (ptrdiff_t cursor = 0; cursor < input_length;) {
+		Report r;
+		get_next_report(&r, &cursor);
+
+		bool inc = r.levels[0] < r.levels[1];
+		for (int j = 0; j < r.level_count - 1; ++j) {
+			int32_t a = r.levels[j];
+			int32_t b = r.levels[j + 1];
 			int32_t d = abs(a - b);
 			if (!(1 <= d && d <= 3) || (inc ^ (a < b))) {
 				goto next_report;
@@ -54,16 +52,18 @@ next_report:
 
 #ifdef GOLD
 	int safe_report_count = 0;
-	for (int i = 0; i < report_count; ++i) {
-		Report const *r = &reports[i];
+	for (ptrdiff_t cursor = 0; cursor < input_length;) {
+		Report r;
+		get_next_report(&r, &cursor);
+
 		int damp_state = 0;
 		int damp = INT_MAX;
 
-		bool inc = (r->levels[0] < r->levels[1]) + (r->levels[1] <
-		    r->levels[2]) + (r->levels[2] < r->levels[3]) > 1;
-		for (int j = 0; j < r->level_count - 2;) {
-			int32_t a = r->levels[j + (damp <= j)];
-			int32_t b = r->levels[j + 1 + (damp <= j + 1)];
+		bool inc = (r.levels[0] < r.levels[1]) + (r.levels[1] <
+		    r.levels[2]) + (r.levels[2] < r.levels[3]) > 1;
+		for (int j = 0; j < r.level_count - 2;) {
+			int32_t a = r.levels[j + (damp <= j)];
+			int32_t b = r.levels[j + 1 + (damp <= j + 1)];
 			int32_t d = abs(a - b);
 			if (!(1 <= d && d <= 3) || (inc ^ (a < b))) {
 				switch (damp_state++) {
@@ -121,43 +121,29 @@ get_input(int argc, char **argv)
 }
 
 static void
-parse_input(void)
+get_next_report(Report *report, ptrdiff_t *cursor)
 {
-	for (ptrdiff_t i = 0; i < input_length;) {
-		for (int j = 0;; ++j) {
-			if (j >= MAX_REPORT_LEVEL_COUNT) {
-				goto fail;
-			}
-			int32_t n = 0;
-			for (;;) {
-				if (i >= input_length) {
-					goto fail;
-				}
-				char c = input[i];
-				if (!('0' <= c && c <= '9')) {
-					break;
-				}
-				n = 10 * n + (c - '0');
-				++i;
-			}
-
-			reports[report_count].levels[j] = n;
-
-			if (input[i++] == '\n') {
-				int count = j + 1;
-				if (count < 4) {
-					goto fail;
-				}
-				reports[report_count].level_count = count;
-				break;
-			}
-		}
-		if (++report_count > MAX_REPORT_COUNT) {
-			goto fail;
+	int level_count = 0;
+	int32_t n = 0;
+	for (ptrdiff_t i = *cursor; i < input_length; ++i) {
+		char c = input[i];
+		if ('0' <= c && c <= '9') {
+			n *= 10;
+			n += c - '0';
+		} else if (c == ' ') {
+			report->levels[level_count] = n;
+			++level_count;
+			n = 0;
+		} else if (c == '\n') {
+			report->levels[level_count] = n;
+			++level_count;
+			report->level_count = level_count;
+			*cursor = i + 1;
+			return;
+		} else {
+			(void)*(char *)0;
 		}
 	}
-	return;
-fail:
 	fprintf(stderr, "parse error\n");
 	exit(EXIT_FAILURE);
 }
