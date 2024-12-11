321f693ec459ad5ee17b70894aa851bf5f940147
Charles Hood <chood@chood.net>
2024-12-10 22:48:23 -0600

    Add 11 starter code

---
+++ 11/11.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <string_view>
+
+static std::string_view get_input(int, char **);
+
+int
+main(int argc, char **argv)
+{
+	std::string_view input = get_input(argc, argv);
+}
+
+static std::string_view
+get_input(int argc, char **argv)
+{
+	if (argc != 2) {
+		fprintf(stderr, "expected 1 argument\n");
+		exit(EXIT_FAILURE);
+	}
+
+	int fd = open(argv[1], O_RDONLY);
+	if (fd < 0) {
+		perror("open");
+		exit(EXIT_FAILURE);
+	}
+
+	off_t length = lseek(fd, 0, SEEK_END);
+	if (length < 0) {
+		perror("lseek");
+		exit(EXIT_FAILURE);
+	}
+	void *data = mmap(nullptr, length, PROT_READ, MAP_PRIVATE, fd, 0);
+	if (data == MAP_FAILED) {
+		perror("mmap");
+		exit(EXIT_FAILURE);
+	}
+	return {static_cast<char const *>(data), static_cast<size_t>(length)};
+}
